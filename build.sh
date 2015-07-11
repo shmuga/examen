e03489da57d54cc05b731204f167f8527f25ecd4
Mark Orel <mail.ormark@gmail.com>
2015-07-11 19:27:57 +0300

    done

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm ./build/*
 libreoffice --headless --convert-to pdf --outdir ./build/ ./src/*.odt
-pdfunite ./build/*.pdf ./build/all.pdf
+pdfunite ./build/*.pdf ./build/xxx.pdf
 libreoffice --headless --convert-to pdf table_of_contents.odt
 cp table_of_contents.pdf ./build/0.pdf
